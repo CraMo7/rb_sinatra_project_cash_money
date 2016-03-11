88cea5ee78740e6490429413675e7be2c59ccfc3
Craig Morton <CraigMorton92@gmail.com>
2016-03-11 14:56:33 +0000

    directory structure & basic operational files created

---
+++ cash_money.sh
@@ -0,0 +1,5 @@
+createdb cash_money;
+psql -d cash_money -f ./db/cash_money_db_setupp.sql;
+ruby ./db/seeds.rb
+for file in specs/*.rb; do echo "#########################"; echo $file; ruby $file; echo $file; echo "#########################"; done
+rerun ./cash_money.rb
