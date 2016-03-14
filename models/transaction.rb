6d25fd03e5094a759443dacdffa70b4780926f45
Craig Morton <CraigMorton92@gmail.com>
2016-03-14 09:53:53 +0000

    transaction contains merchant, category, detail objects. can convert to JSON hash

--- models/transaction.rb
+++ models/transaction.rb
@@ -6,9 +6,36 @@ class Transaction
 
   def initialize(params)
     @id = params["id"]
-    @merchant = params["merchant_object"]
-    @category = params["category_object"]
-    @detail = params["detail_object"]
+    @merchant = params["merchant"]
+    @category = params["category"]
+    @detail = params["detail"]
+  end
+
+  def self.all_tagged(category)
+    
+    
+  end
+
+  def self.all
+
+    
+  end
+
+  def create_json
+    @tranaction_full_hash = {
+      "merchant_id" => @merchant.id.to_s,
+      "merchant_name" => @merchant.name.to_s,
+      "category_id" => @category.id.to_s,
+      "category_name" => @category.name.to_s,
+      "detail_id" => @detail.id.to_s,
+      "detail_description" => @detail.description.to_s,
+      "detail_amount" => @detail.amount.to_s
+    }
+    
+  end
+
+  def save
+    
   end
 
   
--- specs/tranaction_spec.rb
+++ specs/tranaction_spec.rb
@@ -8,17 +8,17 @@ require_relative("../models/detail.rb")
 class TransactionTest < MiniTest::Test
 
   def setup
-    category_params = {
-      "id" => nil,
-      "name" => "foOD",
-    }
-    @category = Category.new(category_params)
-
     merchant_params = {
       "id" => 1,
       "name" => "teSCO"
     }
     @merchant = Merchant.new(merchant_params)
+
+    category_params = {
+      "id" => 1,
+      "name" => "foOD",
+    }
+    @category = Category.new(category_params)
 
     detail_params = {
       "id" => 1,
@@ -28,21 +28,23 @@ class TransactionTest < MiniTest::Test
     @detail = Detail.new(detail_params)
 
     params = {
-      "merchant_object" => @merchant,
-      "category_object" => @category,
-      "detail_object" => @detail
+      "merchant" => @merchant,
+      "category" => @category,
+      "detail" => @detail
     }
     @transaction = Transaction.new(params)
   end
 
+  def test_transaction_contains_merchant_object
+    assert_equal(Merchant, @merchant.class)
+  end
 
+  def test_transaction_contains_category_object
+    assert_equal(Category, @category.class)
+  end
 
-
-  def test_transaction
-
-
-    binding.pry
-    nil
+  def test_transaction_contains_detail_object
+    assert_equal(Detail, @detail.class)
   end
 
 end
